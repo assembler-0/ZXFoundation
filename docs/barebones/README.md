8e45c5a48f4d6315a33d705aafbdf2a4433cd07e
assembler-0 <diaviekone13@gmail.com>
2026-05-05 11:15:17 +0700

    barebones update

---
+++ docs/barebones/README.md
@@ -0,0 +1,8 @@
+# ZXFoundationLoader barebones
+> This directory contains the needed source files to bootstrap a kenrel using ZXFL
+
+## Prerequisites
+- s390x-ibm-linux-gnu gcc
+- ZXFoundationLoader binaries (`core.zxfoundationloader00.sys` and `core.zxfoundationloader01.sys`)
+- hercules tools (dasdload)
+- ZXFoundation kernel tools (gen_checksums)
--- docs/barebones/compile.sh
+++ docs/barebones/compile.sh
@@ -2,13 +2,12 @@
 
 set -e
 
-export CFLAGS="-ffreestanding -nostdlib -msoft-float -mno-vx -m64 -march=z196 -mtune=z196 -pipe -fno-stack-protector -mzarch -static-libgcc -O2 -g0"
-export INC="I../../include/arch/s390x/init/zxfl"
+export CFLAGS="-ffreestanding -nostdlib -msoft-float -mno-vx -m64 -march=z196 -mtune=z196 -pipe -fno-stack-protector -std=c23 -mzarch -static-libgcc -O2 -g0"
 export LDFLAGS="-nostdlib -static --no-dynamic-linker -ztext -zmax-page-size=0x1000 --no-pie -g -melf64_s390"
 
-s390x-ibm-linux-gnu-gcc $CFLAGS -I$INC -isystem -std=c23 -c head64.S -o head64.o
-s390x-ibm-linux-gnu-gcc $CFLAGS -I$INC -isystem -std=c23 -c main.c -o main.o
-s390x-ibm-linux-gnu-gcc $CFLAGS -I$INC -isystem -std=c23 -c zxvl_cksum.c -o zxvl_cksum.o
+s390x-ibm-linux-gnu-gcc $CFLAGS -c head64.S -o head64.o
+s390x-ibm-linux-gnu-gcc $CFLAGS -c main.c -o main.o
+s390x-ibm-linux-gnu-gcc $CFLAGS -c zxvl_cksum.c -o zxvl_cksum.o
 s390x-ibm-linux-gnu-ld -T link.ld $LDFLAGS head64.o main.o zxvl_cksum.o -o core.zxfoundation.nucleus
 ./gen_checksums core.zxfoundation.nucleus
 dasdload -z sysres.conf sysres.3390
